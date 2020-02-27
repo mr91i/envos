9e34905b249602deddec0b9d9b1b11fdc424ac9f
Shoji_aur <shoji9m@gmail.com>
2020-02-27 14:38:39 +0900

    enable read input file

--- pmodes/header.py
+++ pmodes/header.py
@@ -3,11 +3,12 @@
 #  to supress cache files : 
 #      write "PYTHONDONTWRITEBYTECODE = 1" to ~/.bashrc or ~/.bash_profile
 import os
+import sys
+import argparse
 dn_here = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
 dn_home = os.path.abspath(dn_here + '/../')
 dn_radmc = dn_home + "/radmc"
 dn_fig = dn_home + "/fig"
-import sys
 py_version = sys.version_info
 if py_version[0] == 2:
     print('''\n
@@ -17,11 +18,18 @@ Please use and update your Python3 to the newest version.
 At this writing, Python 3.8.1 is the newest.''')
     exit()
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('input_file_name', nargs='?', default="L1527.in")
+args = parser.parse_args()
+print("[header.py] Input file path is ", dn_home+"/"+args.input_file_name)
+
 class read_inp:
     def __init__(self, path):
         with open(path) as f:
             exec(f.read(), {}, self.__dict__)
-inp = read_inp(dn_home+"/L1527.in")
+
+inp = read_inp(dn_home+"/"+args.input_file_name)
+
 
 #or 
 #from .. import "L1527.in" as inp 
