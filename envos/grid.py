92ea8ae698915d685428755b1ae9566ce798a011
Shoji Mori <shoji9m@gmail.com>
2021-02-27 21:20:19 +0900

    add loger

--- envos/grid.py
+++ envos/grid.py
@@ -1,5 +1,7 @@
 import numpy as np
 import envos.nconst as nc
+from envos.log import set_logger
+logger = set_logger(__name__)
 
 
 class Grid:
@@ -20,7 +22,7 @@ class Grid:
         logr=True,
     ):
 
-        if None not in (ri_ax, ti_ax, pi_ax):
+        if (ri_ax is not None) and (ti_ax is not None) and (pi_ax is not None):
             self.ri_ax = ri_ax
             self.ti_ax = ti_ax
             self.pi_ax = pi_ax
@@ -40,6 +42,7 @@ class Grid:
         self.set_cellcenter_axes()
         self.set_meshgrid()
         self.set_cylyndrical_coord()
+        self.show_grid_info()
 
     def set_cellcenter_axes(self):
         self.rc_ax = 0.5 * (self.ri_ax[0:-1] + self.ri_ax[1:])
@@ -81,3 +84,47 @@ class Grid:
 
         self.ti_ax = np.linspace(*theta_lim, ntheta + 1)
         self.pi_ax = np.linspace(*phi_lim, nphi + 1)
+
+    def show_grid_info(self):
+        ri = self.ri_ax / nc.au
+        ti = np.rad2deg(self.ti_ax)
+        pi = np.rad2deg(self.pi_ax)
+        logger.info(f"Grid:")
+        logger.info(f"    r  = [{ri[0]:.2f}:{ri[-1]:.2f}] au")
+        logger.info(f"    Nr = {len(ri)-1}")
+        logger.info(f"    θ  = [{ti[0]:.2f}:{ti[-1]:.2f}] ")
+        logger.info(f"    Nθ = {len(ti)-1}")
+        logger.info(f"    φ  = [{pi[0]:.2f}:{pi[-1]:.2f}] ")
+        logger.info(f"    Nφ = {len(pi)-1}")
+        logger.info("")
+
+
+def get_interface_coord(
+    rau_lim=None,
+    theta_lim=(0, np.pi / 2),
+    phi_lim=(0, 2 * np.pi),
+    nr=None,
+    ntheta=None,
+    nphi=1,
+    dr_to_r=None,
+    aspect_ratio=1.0,
+    logr=True,
+):
+
+    if dr_to_r is not None:
+        nr = int(np.log(rau_lim[1] / rau_lim[0]) / dr_to_r)
+        ntheta_float = (
+            (theta_lim[1] - theta_lim[0]) / dr_to_r / aspect_ratio
+        )
+        ntheta = int(round(ntheta_float))
+
+    if logr:
+        ri_ax = np.geomspace(*rau_lim, nr + 1) * nc.au
+    else:
+        ri_ax = np.linspace(*rau_lim, nr + 1) * nc.au
+
+    ti_ax = np.linspace(*theta_lim, ntheta + 1)
+    pi_ax = np.linspace(*phi_lim, nphi + 1)
+
+    return ri_ax, ti_ax, pi_ax
+
