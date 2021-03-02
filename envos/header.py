2c45944fea321f2970d3e9aff2e5c7a2b57532d6
Shoji Mori <shoji9m@gmail.com>
2021-03-03 07:41:22 +0900

    enable install

---
+++ envos/header.py
@@ -0,0 +1,21 @@
+## Check Python version
+import sys
+
+py_version = sys.version_info
+if py_version[0] + py_version[1] * 0.1 < 3.3:
+    print(
+        """
+This module does not work with Python2 or <3.5 .
+At 1 Jan 2020, most packages stopped to support Python2 (see https://python3statement.org).
+Please use Python3 and update your Python3 to the newest version.
+I have confirmed that package works in Python 3.8.1."""
+    )
+    raise Exception("Too old version of Python")
+
+print(
+    """
+====================================
+         Running envos.py
+====================================
+"""
+)
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='envos',
+    version='0.1.0',
+    license='GNU General Public License v3.0',
+    description='Envelope observation simulator',
+    install_requires=["numpy", "matplotlib", "pandas", "astropy", "scipy", "dataclasses"],
+    author='Shoji Mori',
+    author_email='shoji9m@gmail.com',
+    url='https://github.com/mr91i/envos',
+#    packages=find_packages("envos"),
+#    package_dir={'': 'envos'},
+    python_requires='>=3.6, <4',
+
+)
