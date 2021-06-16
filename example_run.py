5abee56e98950bacd8f8195041c0661bde2f8139
Shoji Mori <shoji9m@gmail.com>
2021-06-16 10:03:32 +0900

    modif

--- example_run.py
+++ example_run.py
@@ -35,60 +35,60 @@ def main():
     )
 
 
-    # Printing a Config class shows all arguments set in the instance, 
+    # Printing a Config class shows all arguments set in the instance,
     # including implicitly set arguments.
     print(config)
-    
-    
-    # ModelGenerator generates a physical model with calculating 
-    # density, velocity, and temperature structure. The simplest way 
+
+
+    # ModelGenerator generates a physical model with calculating
+    # density, velocity, and temperature structure. The simplest way
     # to pass the argumets to ModelGenerator is just to pass a config class.
     mg = envos.ModelGenerator(config)
-    
-    # Calculate density and velocity structure. 
+
+    # Calculate density and velocity structure.
     mg.calc_kinematic_structure()
-    
+
     # Calculate temperature structure.
     mg.calc_thermal_structure()
-    
-    # Generate the physical model as a `Circumstellar Model` object. 
+
+    # Generate the physical model as a `Circumstellar Model` object.
     model = mg.get_model()
-    
+
     # Once you save a data file as:
     #   model.save_pickle("model.pkl")
     # you can read the model as
     #   model = envos.read_model("run/model.pkl")
-    
+
     # "Circumstellar Model" object is printed as a readable format.
     # For n-dimension array, to be readable, only shape, min, and max of the array are printed.
-    # Learn what variables are available, and check if the calculation is done collectly. 
+    # Learn what variables are available, and check if the calculation is done collectly.
     print(model)
-    
-    
+
+
     # ObsSimulator executes the calculation of synthetic observation.
-    # ObsSimulator can also get recieve a `Config` object. 
+    # ObsSimulator can also get recieve a `Config` object.
     osim = envos.ObsSimulator(config)
-    
+
     # ObsSimulator set radmc-3d input files again when it recieves a physical model,
     # unless input files used in calculaitng the thermal structure is used.
     # For example, if you want to understand which region corresponds to which intensity feature,
-    # limiting emitting regions after calculating the temperture structure may help you. 
+    # limiting emitting regions after calculating the temperture structure may help you.
     osim.set_model(model)
-    
+
     # observe_line() calculates the radiative transfer with ray tracing method,
-    # and returns a ObsData3D object, which contains intensity distribution in 
+    # and returns a ObsData3D object, which contains intensity distribution in
     # 3-dimension (longitude, latitude, and velocity; i.e., 3D data cube).
     odat = osim.observe_line()
-    
+
     # Once you save a data file as:
     #   odat.save_instance(filename="lineobs.pkl")
     # you can read the data as:
     #   odat = envos.read_obsdata("run/lineobs.pkl")
 
-    
+
     PV = odat.get_PV_map(pangle_deg=0)
     print(PV)
-    
+
     # To visualize the PV diagram, one can use "envos.plot_tools"
     # which provide functions to plot results.
     envos.plot_tools.plot_pvdiagram(PV)
