cc1be4ee76f4c948aa6d1fcf9dcd010152e3ccd8
mr91i <shoji9m@gmail.com>
2022-11-01 17:18:01 +0900

    add column_density

---
+++ envos/column_density.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+import envos
+from . import log
+from .nconst import au
+from scipy import interpolate, integrate
+
+#from myplot import ini
+
+def calc_column_density(model, direction, double_interp=False):
+    if direction == "r":
+        return integrate.cumulative_trapezoid(model.rhogas, model.rr, axis=0, initial=0)
+    elif direction == "theta":
+        return integrate.cumulative_trapezoid(model.rhogas, model.tt, axis=1, initial=0) * model.rr
+    elif direction == "z":
+        if double_interp:
+            x = np.linspace(0, np.max(model.rc_ax), 1000)
+            z = x
+            xx, zz = np.meshgrid(x, z, indexing="ij")
+            
+            newgrid = np.stack([np.sqrt(xx**2+zz**2), np.arctan2(xx, zz)], axis=-1)
+            rho_xz = interpolate.interpn(
+                (model.rc_ax, model.tc_ax),
+                model.rhogas[..., 0],
+                newgrid,
+                bounds_error=False,
+                fill_value=None,
+            )
+
+            colz = -integrate.cumulative_trapezoid(rho_xz[:,::-1], zz[:,::-1], axis=1, initial=0)[:,::-1]
+
+            col = interpolate.interpn(
+                (x, z),
+                colz,
+                np.stack([model.R, model.z], axis=-1),                 
+                bounds_error=False,
+                fill_value=None,
+            )
+
+            print(col.shape, np.max(col), np.min(col))
+            #exit()
+            return col
+
+        else:
+            "  Samller grid noise in the interpolation  "
+            log.logger.info("Computing column density in z-direction, which could take time...")    
+            rho_interp = interpolate.RegularGridInterpolator(
+                (model.rc_ax, model.tc_ax),
+                model.rhogas[..., 0],
+                bounds_error=False,
+                fill_value=None,
+            )    
+            zlim = np.max(model.z) * 1.1
+        return v_column_density_z(model.R, model.z, model, rho_interp, zlim).astype(float)
+    else:
+        raise ValueError(f"Wrong input direction: {direction}")
+
+
+def test_column_density(model):
+    print(model)
+    print(model.rhogas.shape)
+    test_colr = 0
+    test_colt = 0
+    test_colz = 1
+
+    # 1. density integration along r-direction 
+    if test_colr:
+        colr = integrate.cumulative_trapezoid(model.rhogas, model.rr, axis=0, initial=0)
+        plt.pcolormesh(model.R[...,0], model.z[...,0], colr[...,0], shading="nearest")
+        plt.show()
+
+    # 2. density integration along theta-direction 
+    if test_colt:
+        colt = integrate.cumulative_trapezoid(model.rhogas, model.tt, axis=1, initial=0) * model.rr
+        plt.pcolormesh(model.R[...,0], model.z[...,0], colt[...,0], shading="nearest")
+        plt.show()
+
+    # 3. density integration along z-direction 
+    if test_colz:
+        rho_interp = interpolate.RegularGridInterpolator(
+            (model.rc_ax, model.tc_ax),
+            model.rhogas[..., 0],
+            bounds_error=False,
+            fill_value=None,
+        )    
+    
+        zlim = np.max(model.z)*1.1
+        colz = v_column_density_z(model.R, model.z, model, rho_interp, zlim).astype(float)
+        print(colz.shape)
+        plt.pcolormesh(model.R[...,0], model.z[...,0], colz[...,0], shading="nearest")
+        plt.show()
+
+
+def column_density_z(R, z, model, interp_func, zlim):
+    _R = R
+    _z_ax = np.arange(z, zlim, 0.1*z)
+    r = np.sqrt(_R**2 + _z_ax**2)
+    t = np.arctan2(_R, _z_ax)
+    points = np.stack([r, t], axis=-1)
+    #print(points)
+    rho = interp_func(points)
+    #print(rho)
+    colz = integrate.simpson(rho, _z_ax)
+    #print(colz)
+    return colz
+
+v_column_density_z = np.frompyfunc(column_density_z, 5, 1)
+
+if __name__=="__main__":
+    config = envos.Config(
+        run_dir="./run",
+        n_thread=10,
+        nphot=1e5,
+        rau_in=10,
+        rau_out=1000,
+        dr_to_r=0.01,
+        aspect_ratio=1,
+        inenv="UCM",
+        CR_au=100,
+        Ms_Msun=0.3,
+        T=10,
+        cavangle_deg=45,
+        f_dg=0.01,
+        opac="MRN20",
+        Lstar_Lsun=1.0,
+    )
+    mg = envos.ModelGenerator(config)
+    mg.calc_kinematic_structure()
+    mg.calc_thermal_structure()
+    model = mg.get_model()
+
+    test_column_density(model)
